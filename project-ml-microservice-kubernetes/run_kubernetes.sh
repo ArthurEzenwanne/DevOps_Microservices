59d1d174b2a10cd4593d55841075de1279ac4667
EC2 Default User <ec2-user@ip-172-31-18-162.ec2.internal>
2023-02-06 11:46:07 +0000

    feat: Add kubernetes and docker functionalities

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,19 +4,21 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
 dockerpath=arthurezenwanne/ml-microservice-k8
 
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run arthurezenwanne --image=$dockerpath --port=80
+minikube kubectl -- run predict-app --image=$dockerpath --port=80
+# kubectl run PredictApp --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
-kubectl get pods
+minikube kubectl -- get pods
+# kubectl get pods
 
 # Step 4:
-# Forward the container port to a host
-kubectl port-forward arthurezenwanne 8000:80 
-
+# Forward the container port to a host, ensure pods are up
+sleep 120
+minikube kubectl -- port-forward predict-app 8000:80 
+# kubectl port-forward PredictApp 8000:80
